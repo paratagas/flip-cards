a8074001571493b4bfd5b6468b141ecb9d4e0f17
paratagas <paratagas@gmail.com>
2020-04-05 13:20:26 +0200

    Create base structure.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import purple_back from './cards/purple_back.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+/* eslint-disable react/prefer-stateless-function */
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-container">
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+      	  <div className="card-item">
+      	    <img src={ purple_back } className="card-img" alt="card-img" />
+      	  </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
