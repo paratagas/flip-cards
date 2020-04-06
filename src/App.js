1b769baa0f0481b8a842edae42b1bb8ca5a94f41
paratagas <paratagas@gmail.com>
2020-04-06 14:11:02 +0200

    Improve components structure, set handlers.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,41 @@ import React, { Component } from 'react';
 import './App.scss';
 import Card from './components/Card/Card';
 
+const cardsAmount = 12;
+const CARDS = Array.from(Array(cardsAmount).keys());
+
 /* eslint-disable react/prefer-stateless-function */
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // bindings:
+    this.createCardsList = this.createCardsList.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  createCardsList() {
+    const cardsList = CARDS.map((item, index) => {
+      return (
+        <Card
+          key={`card-${ index }`}
+          onClickHandler={ this.handleClick }
+        />
+      );
+    });
+
+    return cardsList;
+  }
+
+  handleClick() {
+    console.log('Click');
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App__container">
-
-      	  <Card />
-      	  <Card />
-          <Card />
-      	  <Card />
-      	  <Card />
-      	  <Card />
-          <Card />
-      	  <Card />
-      	  <Card />
-      	  <Card />
-          <Card />
-      	  <Card />
-
+          { this.createCardsList() }
         </div>
       </div>
     );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,28 @@
  */
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import purple_back from '../../cards/purple_back.png';
 import './Card.scss';
 
 /* eslint-disable react/prefer-stateless-function */
 class Card extends Component {
+  static propTypes = {
+    onClickHandler: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onClickHandler: () => {},
+  };
+
   render() {
+    const { onClickHandler } = this.props;
+
     return (
-      <div className="card-item">
+      <div
+        className="card-item"
+        onClick={ () => onClickHandler() }
+      >
         <img src={ purple_back } className="card-item__img" alt="card-item__img" />
       </div>
     );
