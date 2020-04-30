a09f1c3dc28b95a8256d74072d853ce0006e9b13
paratagas <paratagas@gmail.com>
2020-04-30 13:17:36 +0200

    Code refactor.

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,10 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 /* eslint-disable no-undef */
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders a message', () => {
+  const { container } = render(<App />);
+  
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <div className="App__container">
+  `)
+})
